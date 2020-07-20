b9af1c6a2d2ee9173b0deb2dbd3c3a175a9acb22
mustafaatush <mustafaatush@gmail.com>
2020-07-19 22:27:36 -0700

    set up gradle and resources

--- app/src/main/java/com/kodyuzz/kanas/MainActivity.kt
+++ app/src/main/java/com/kodyuzz/kanas/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.kodyuzz.kanas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
